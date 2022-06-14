4c7887006369c8bb66b30b4971a1028c94623c25
Colin Dembovsky <colindembovsky@gmail.com>
2022-06-14 16:56:56 +0000

    Formatting

--- samples/BikeSharingApp/Infrastructure/main.tf
+++ samples/BikeSharingApp/Infrastructure/main.tf
@@ -1,60 +1,60 @@
 # Generate random resource group name
 resource "azurerm_resource_group" "rg" {
-  name      = var.resource_group_name
-  location  = var.resource_group_location
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
 resource "random_id" "log_analytics_workspace_name_suffix" {
-    byte_length = 8
+  byte_length = 8
 }
 
 resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
-    # the workSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
-    location            = var.log_analytics_workspace_location
-    resource_group_name = azurerm_resource_group.rg.name
-    sku                 = var.log_analytics_workspace_sku
+  # the workSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
+  name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
+  location            = var.log_analytics_workspace_location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = var.log_analytics_workspace_sku
 }
 
 resource "azurerm_log_analytics_solution" "container-insights" {
-    solution_name         = "ContainerInsights"
-    location              = azurerm_log_analytics_workspace.log_analytics_workspace.location
-    resource_group_name   = azurerm_resource_group.rg.name
-    workspace_resource_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
-    workspace_name        = azurerm_log_analytics_workspace.log_analytics_workspace.name
+  solution_name         = "ContainerInsights"
+  location              = azurerm_log_analytics_workspace.log_analytics_workspace.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  workspace_name        = azurerm_log_analytics_workspace.log_analytics_workspace.name
 
-    plan {
-        publisher = "Microsoft"
-        product   = "OMSGallery/ContainerInsights"
-    }
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-    name                = var.cluster_name
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    dns_prefix          = var.dns_prefix
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
 
-    default_node_pool {
-        name            = "agentpool"
-        node_count      = var.agent_count
-        vm_size         = "Standard_D2_v2"
-    }
+  default_node_pool {
+    name       = "agentpool"
+    node_count = var.agent_count
+    vm_size    = "Standard_D2_v2"
+  }
 
-    identity {
-        type = "SystemAssigned"
-    }
+  identity {
+    type = "SystemAssigned"
+  }
 
-    oms_agent {
-        log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
-    }
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  }
 
-    network_profile {
-        load_balancer_sku = "Standard"
-        network_plugin = "kubenet"
-    }
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin    = "kubenet"
+  }
 
-    tags = {
-        Application = "Mindaro"
-    }
+  tags = {
+    Application = "Mindaro"
+  }
 }
--- samples/BikeSharingApp/Infrastructure/variables.tf
+++ samples/BikeSharingApp/Infrastructure/variables.tf
@@ -1,42 +1,42 @@
 variable "resource_group_location" {
-  default       = "southcentralus"
-  description   = "Location of the resource group."
+  default     = "southcentralus"
+  description = "Location of the resource group."
 }
 
 variable "agent_count" {
-    default = 2
+  default = 2
 }
 
 variable "ssh_public_key" {
-    default = "~/.ssh/id_rsa.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "dns_prefix" {
-    default = "mindaro"
+  default = "mindaro"
 }
 
-variable cluster_name {
-    default = "mindaro"
+variable "cluster_name" {
+  default = "mindaro"
 }
 
-variable resource_group_name {
-    default = "mindaro"
+variable "resource_group_name" {
+  default = "mindaro"
 }
 
-variable location {
-    default = "South Central US"
+variable "location" {
+  default = "South Central US"
 }
 
-variable log_analytics_workspace_name {
-    default = "AKSLogAnalyticsWorkspace"
+variable "log_analytics_workspace_name" {
+  default = "AKSLogAnalyticsWorkspace"
 }
 
 # refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
-variable log_analytics_workspace_location {
-    default = "southcentralus"
+variable "log_analytics_workspace_location" {
+  default = "southcentralus"
 }
 
 # refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
-variable log_analytics_workspace_sku {
-    default = "PerGB2018"
+variable "log_analytics_workspace_sku" {
+  default = "PerGB2018"
 }
