eac2b50b8e726cdcec818397dfd259c9721bb08a
Colin Dembovsky <colindembovsky@gmail.com>
2022-09-28 21:10:57 +0000

    Fixing reporting

--- samples/BikeSharingApp/Bikes/jest.config.js
+++ samples/BikeSharingApp/Bikes/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
     coveragePathIgnorePatterns: ['/node_modules/'],
+    reporters: [
+        'default',
+        ['jest-junit', {outputDirectory: 'reports', outputName: 'report.xml'}],
+    ],
+    collectCoverage: true,
+    "coverageReporters": ["text", "cobertura" ]
 };
